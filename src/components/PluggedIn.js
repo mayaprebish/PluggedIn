4e073dfc2e7d90950c8f726c3e5c356b38e9ce9f
Maya Prebish <prebish.m@husky.neu.edu>
2019-11-28 11:24:30 -0500

    jumbotron

--- src/components/PluggedIn.js
+++ src/components/PluggedIn.js
@@ -5,19 +5,19 @@ import './PluggedIn.css';
 import Header from "./Header";
 
 export default class PluggedIn extends React.Component {
-
-    imageUrl = "https://live.staticflickr.com/65535/49124977911_fae20e68a8_o.jpg"
-
     render() {
         return (
             <Router>
-                <div className="header">
-                    <Header/>
-                </div>
-
                 <div className="content">
-                    <img src={this.imageUrl} width="100%"/>
+                    <div className="header">
+                        <Header/>
+                    </div>
 
+                    <div className="jumbotron">
+                        <h1 className="display-4">Welcome to PluggedIn!</h1>
+                        <p className="lead">A resource for artists and tour managers to find venues</p>
+
+                    </div>
                 </div>
             </Router>
         )
