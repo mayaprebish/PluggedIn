85e78a611db535ae075acfc21fb34ab8e81ad676
molambo <lambo.m@husky.neu.edu>
2019-12-08 23:36:28 -0500

    Added state

--- src/components/Register.js
+++ src/components/Register.js
@@ -6,7 +6,9 @@ import {Link} from "react-router-dom";
 export default class Register extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+          user: ''
+        }
     }
 
     componentDidMount() {
